795679ec150373981ebadf25ba29790b81bc4545
Getinet Asimare <getinetasimare@gmail.com>
2025-06-06 12:07:54 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,2 @@
 const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log('undefined is undefined');
-} else if (args.length === 1) {
-  console.log(args[0]);
-} else {
-  console.log(`${args[0]} is ${args[1]}`);
-}
+console.log(`${args[0]} is ${args[1]}`);
