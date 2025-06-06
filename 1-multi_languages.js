bb7588520de823468b2ecc6f319f46843a28cf04
Getinet Asimare <getinetasimare@gmail.com>
2025-06-06 11:12:29 +0300

    Create 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,11 @@
+// Create a constant variable for the third line.
+const myVar = "JavaScript is amazing";
+
+// Print the first line.
+console.log("C is fun");
+
+// Print the second line.
+console.log("Python is cool");
+
+// Print the third line using the constant variable.
+console.log(myVar);
