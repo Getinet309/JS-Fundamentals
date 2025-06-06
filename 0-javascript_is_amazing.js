a9efe29c178c5dd2e61dcf677e42e7966b5b9128
Getinet Asimare <getinetasimare@gmail.com>
2025-06-06 11:07:26 +0300

    Create 0-javascript_is_amazing.js

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+// Create a constant variable named myVar with the specified value.
+const myVar = "JavaScript is amazing";
+
+// Use console.log() to print the value of myVar to the console.
+console.log(myVar);
