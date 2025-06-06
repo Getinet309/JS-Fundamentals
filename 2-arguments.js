da589afce19b3d1086f16a909f58ab3f8e40cce3
Getinet Asimare <getinetasimare@gmail.com>
2025-06-06 11:15:52 +0300

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const numArguments = process.argv.length - 2;
+
+// Check the number of arguments and print the corresponding message.
+if (numArguments === 0) {
+  console.log("No argument");
+} else if (numArguments === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
