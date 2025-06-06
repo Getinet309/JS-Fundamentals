ac8b312e5aa62612f494ba5f2eebaff644bd018d
Getinet Asimare <getinetasimare@gmail.com>
2025-06-06 11:18:10 +0300

    Create 3-value_argument.js

---
+++ 3-value_argument.js
@@ -0,0 +1,7 @@
+if (process.argv[2] !== undefined) {
+  // If the first argument exists, print its value.
+  console.log(process.argv[2]);
+} else {
+  // If no arguments are provided (i.e., process.argv[2] is undefined), print "No argument".
+  console.log("No argument");
+}
