f76e262cc4bd8cf52ec7a9c646aee7bcbc846449
Getinet Asimare <getinetasimare@gmail.com>
2025-06-06 11:20:13 +0300

    Create 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,4 @@
+if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
+  // Print the two arguments in the format: "arg1 is arg2"
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+}
